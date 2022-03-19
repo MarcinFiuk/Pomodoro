300c5e715dec92c64b6459cc001ea0cbd6ebeeae
Marcin_Fi <fiukmarcinn@gmail.com>
2022-03-19 13:03:32 +0000

    refactor: input changed to uncontrolled and HTML input validation added

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -3,15 +3,10 @@ import useTimer from './../../hooks/useTimer';
 import fromSecToMin from '../../utils/fromSecToMin';
 import TimerInput from './../Input/TimerInput';
 
-const invalidChars = ['-', '+', 'e', 'E'];
-
 const Timer = () => {
     const [pomodoro, setPomodoro] = useState(60);
-    const [validatePomodoroInput, setValidatePomodoroInput] = useState(60);
     const [shortBreak, setShortBreak] = useState(10);
-    const [validateShortBreakInput, setValidateShortBreakInput] = useState(10);
     const [longBreak, setLongBreak] = useState(20);
-    const [validateLongBreakInput, setValidateLongBreakInput] = useState(20);
 
     const { timer, isRunning, setIsRunning } = useTimer(
         pomodoro,
@@ -19,24 +14,16 @@ const Timer = () => {
         longBreak
     );
 
-    const onChangeHandler = (e) => {
-        if (invalidChars.includes(e.nativeEvent.data)) return;
-        //NOTE: input accepts invalidChars as first element, e does not trigger
-        let time = e.target.value;
-        if (e.target.value > 60) time = 60;
-        if (e.target.value < 0) time = 0;
-
-        if (e.target.id === 'pomodoro') setValidatePomodoroInput(time);
-        if (e.target.id === 'short break') setValidateShortBreakInput(time);
-        if (e.target.id === 'long break') setValidateLongBreakInput(time);
-    };
-
     const submitHandler = (e) => {
         e.preventDefault();
 
-        setPomodoro(validatePomodoroInput);
-        setShortBreak(validateShortBreakInput);
-        setLongBreak(validateLongBreakInput);
+        const pomodoroTime = parseInt(e.target['pomodoro'].value);
+        const shortBreakTime = parseInt(e.target['short break'].value);
+        const longBreakTime = parseInt(e.target['long break'].value);
+
+        setPomodoro(pomodoroTime);
+        setShortBreak(shortBreakTime);
+        setLongBreak(longBreakTime);
     };
 
     const timerStartHandler = () => {
@@ -50,21 +37,9 @@ const Timer = () => {
     return (
         <>
             <form onSubmit={submitHandler}>
-                <TimerInput
-                    label='pomodoro'
-                    onChange={onChangeHandler}
-                    value={validatePomodoroInput}
-                />
-                <TimerInput
-                    label='short break'
-                    onChange={onChangeHandler}
-                    value={validateShortBreakInput}
-                />
-                <TimerInput
-                    label='long break'
-                    onChange={onChangeHandler}
-                    value={validateLongBreakInput}
-                />
+                <TimerInput label='pomodoro' />
+                <TimerInput label='short break' />
+                <TimerInput label='long break' />
                 <button type='submit'>Submit</button>
             </form>
             <div>
